c7120aa68cbb2e02290ca85a2450f84f0ba1354a
niniack <nsa325@nyu.edu>
2020-07-08 01:44:20 -0400

    using inbuilt 2d->1d flattening

--- src/edge_detection.rs
+++ src/edge_detection.rs
@@ -1,7 +1,6 @@
 use ndarray::prelude::*;
 use fastblur::gaussian_blur_asymmetric_single_channel;
-
-use crate::utils;
+use std::iter::FromIterator;
 
 pub fn canny(input: Array2<u8>, low_threshold: f32, high_threshold: f32) {
 
@@ -13,12 +12,12 @@ pub fn canny(input: Array2<u8>, low_threshold: f32, high_threshold: f32) {
 
     // Apply Gaussian blur
     let shape = input.dim();
-    let mut blurred = utils::flatten(input).to_vec();
+    let mut blurred = Array::from_iter(input.iter().cloned()).to_vec();
     gaussian_blur_asymmetric_single_channel(&mut blurred, shape.1, shape.0, 5.0, 5.0);
     // utils::write_gray_image("blur.ppm", &data, shape.1 as usize, shape.0 as usize).unwrap();
 
     // Find intensity gradients
-    
+
 
 
 
